f21b17eff0eb8053d99c4109678a2eee40a2f706
rushanthbala <58292966+rushanthbala@users.noreply.github.com>
2021-03-21 22:43:33 +0530

    secruitsecurity purpose

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/app';
 // import * as firebase from 'firebase';
 import 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyDPTma4tP5ITlmIyVXW2Jazh3YyDkpxtsI",
-    authDomain: "whatapp-clone-r1.firebaseapp.com",
-    databaseURL: "https://whatapp-clone-r1-default-rtdb.firebaseio.com",
-    projectId: "whatapp-clone-r1",
-    storageBucket: "whatapp-clone-r1.appspot.com",
-    messagingSenderId: "427305687817",
-    appId: "1:427305687817:web:926f0d2e4244053128d1d8"
+    apiKey: "+++++++++",
+    authDomain: "+++++++++",
+    databaseURL:"+++++++++",
+    projectId: "+++++++++",
+    storageBucket: "+++++++++",
+    messagingSenderId: "+++++++++",
+    appId: "+++++++++",
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
